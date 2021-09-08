790caf8228615754a4cf447786215f345740342b
Chris <christhehorizon@gmail.com>
2021-09-07 22:30:23 -0700

    load pokemon

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -2,25 +2,27 @@ import React, { useState, useEffect} from 'react'
 
 const Pokemon = () => {
 
+    // Returns two variables inside of an array
+    // first variable is the current state we are using (data)
+    // second variable is the method we use to update our state (update data)
     const[ pokemon, setPokemon] = useState([]);
 
-    useEffect(() => {
+    // useEffect(() => {
+    const getPoke = (e) => {
         fetch('https://pokeapi.co/api/v2/pokemon/')
         .then(response => response.json())
         .then(response => setPokemon(response.results))
         .catch(err=> console.log(err))
-    }, []);
+    }
+    // }, []);
 
     const clickHandler = () =>{
-        setPokemon({
-            ...pokemon,
-        })
         console.log(pokemon)
     }
 
     return (
         <div>
-            <button onClick={clickHandler}>Fetch Pokemon</button>
+            <button onClick={getPoke}>Fetch Pokemon</button>
 
             {pokemon.length > 0 && pokemon.map((pokemon, index) => {
                 return( <div key={index}>{pokemon.name}</div>)
